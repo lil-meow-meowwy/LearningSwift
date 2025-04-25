8596609894376c95fa6ccdd764bb4d5915eb8265
luciel. <91286203+lil-meow-meowwy@users.noreply.github.com>
2025-04-25 13:37:40 +0300

    Create CountingValues.swift

---
+++ CountingValues.swift
@@ -0,0 +1,7 @@
+let colours = ["red", "green", "blue", "pink", "yellow", "pink", "orange", "purple", "black", "white", "red"]
+
+var colourCount = colours.count
+print("The number of colours is: \(colourCount)")
+
+var uniqueColours = Set(colours).count
+print("The number of unique colours in array is: \(colourCount)")
